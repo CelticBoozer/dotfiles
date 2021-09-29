1cdfebed3d1f26f3bf3633d7dae379b138a7f1c0
CelticBoozer <cyxapb359@gmail.com>
2021-09-29 05:07:18 +0300

    Waybar: added bashtop on-click ever for cpu plugin. Removed nvim. Added bin directory and bin with random background chnaging

---
+++ bin/randomBackground.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+swaybg -i $(find ~/.wallpaper/. -type f | shuf -n1) -m fill &
+OLD_PID=$!
+while true; do
+    sleep 900
+    swaybg -i $(find ~/.wallpaper/. -type f | shuf -n1) -m fill &
+    NEXT_PID=$!
+    sleep 5
+    kill $OLD_PID
+    OLD_PID=$NEXT_PID
+done
