c4356872c2c02dd5c13f1a0d9104b82827bdc06d
CelticBoozer <cyxapb359@gmail.com>
2022-11-14 17:50:19 +0300

    Added tlp config. #55 implemented.

--- .bin/move-system-configs.sh
+++ .bin/move-system-configs.sh
@@ -3,3 +3,4 @@
 cp ../.system-config-backup/pacman/create-backup.hook /usr/share/libalpm/hooks/create-backup.hook
 cp ../.system-config-backup/pacman/pacman.conf /etc/pacman.conf
 cp ../.system-config-backup/systemd/logind.conf /etc/systemd/logind.conf
+cp ../.system-config-backup/tlp.conf /etc/tlp.conf
