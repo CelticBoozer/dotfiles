a20bd4c7ab207763637d20e98bd959f47f800904
CelticBoozer <cyxapb359@gmail.com>
2022-12-16 04:41:48 +0300

    #67 implemented

--- .bin/move-system-configs.sh
+++ .bin/move-system-configs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cp ../.system-config-backup/pacman/create-backup.hook /usr/share/libalpm/hooks/create-backup.hook
+cp ../.system-config-backup/pacman/create-aur-backup.hook /usr/share/libalpm/hooks/create-aur-backup.hook
 cp ../.system-config-backup/pacman/pacman.conf /etc/pacman.conf
 cp ../.system-config-backup/systemd/logind.conf /etc/systemd/logind.conf
 cp ../.system-config-backup/tlp.conf /etc/tlp.conf
