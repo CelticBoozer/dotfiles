7097ce6fab3981dd4f24f1d6b084a4660b7d768d
CelticBoozer <celtic.boozer@proton.me>
2024-01-21 20:45:37 +0300

    gg

--- .config/nvim/lua/plugin/lsp/trouble.lua
+++ .config/nvim/lua/plugin/lsp/trouble.lua
@@ -1,7 +1,14 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
+	opts = {
+		action_keys = {
+			open_split = { "<c-s>" }, -- open buffer in new split
+			open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+			open_tab = { "<c-t>" },
+		},
+		win_config = { border = "rounded" },
+	},
 	keys = {
 		{ "<leader>d", ":Trouble<CR>", desc = "trouble diagnostics" },
 	},
