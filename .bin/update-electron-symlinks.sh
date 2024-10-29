b8b9791bbcbb5931932140d9bc668156fa243f0f
CelticBoozer <celtic.boozer@proton.me>
2024-10-29 13:50:41 +0300

    style: add newline at the EOF's
    Impelement #257

--- .bin/update-electron-symlinks.sh
+++ .bin/update-electron-symlinks.sh
@@ -4,22 +4,22 @@
 
 # Create a symlink if one does not exist for the corresponding package
 for package in $(pacman -Qq | grep '^electron[0-9]*$'); do
-	if [ ! -L "/home/celtic/.config/$package-flags.conf" ]; then
-		ln -s "/home/celtic/.config/electron-flags.conf" "/home/celtic/.config/$package-flags.conf"
-		echo -e "\e[32mCreated symlink for $package...\e[0m"
-	else
-		echo -e "\e[36mSymlink for $package exists...\e[0m"
-	fi
+  if [ ! -L "/home/celtic/.config/$package-flags.conf" ]; then
+    ln -s "/home/celtic/.config/electron-flags.conf" "/home/celtic/.config/$package-flags.conf"
+    echo -e "\e[32mCreated symlink for $package...\e[0m"
+  else
+    echo -e "\e[36mSymlink for $package exists...\e[0m"
+  fi
 done
 
 # Remove the link if the corresponding package is missing
 for file in "/home/celtic/.config"/electron[0-9]*-flags.conf; do
-	# Extracting a package name from a file name
-	package=$(basename "$file" "-flags.conf")
+  # Extracting a package name from a file name
+  package=$(basename "$file" "-flags.conf")
 
-	# Check if the package exists
-	if ! pacman -Q "$package" >/dev/null; then
-		echo -e "\e[31mPackage $package not found, deleting $file...\e[0m"
-		rm "$file"
-	fi
+  # Check if the package exists
+  if ! pacman -Q "$package" >/dev/null; then
+    echo -e "\e[31mPackage $package not found, deleting $file...\e[0m"
+    rm "$file"
+  fi
 done
